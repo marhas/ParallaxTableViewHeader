e10f29ec1aa03b36e8ea15d6ee93bc9a51145b30
Marcel Hasselaar <marcel@unbadapps.com>
2017-12-11 10:25:44 +0100

    Initial Commit

---
+++ ParallaxTableHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParallaxTableHeader
+//
+//  Created by Marcel Hasselaar on 2017-12-11.
+//  Copyright © 2017 Marcel Hasselaar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
