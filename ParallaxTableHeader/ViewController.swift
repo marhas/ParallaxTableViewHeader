108b8da63bff25581309a6df7a6c0bfa29df1a90
Marcel Hasselaar <marcel@unbadapps.com>
2017-12-11 13:51:24 +0100

    Parallax table header

--- ParallaxTableHeader/ViewController.swift
+++ ParallaxTableHeader/ViewController.swift
@@ -5,21 +5,51 @@
 //  Created by Marcel Hasselaar on 2017-12-11.
 //  Copyright © 2017 Marcel Hasselaar. All rights reserved.
 //
+// Photo by Quino Al on Unsplash
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var tableHeaderContainer: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 50
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        let topConstraint = imageView.topAnchor.constraint(equalTo: view.topAnchor)
+        topConstraint.isActive = true
+
+        let relativeHeight = tableView.bounds.height / 4.5
+        if let tableHeaderView = tableView.tableHeaderView {
+            tableView.tableHeaderView!.bounds = CGRect(origin: tableHeaderView.bounds.origin, size: CGSize(width: tableHeaderView.bounds.size.width, height: relativeHeight))
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "proto1")!
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
     }
-
-
 }
 
